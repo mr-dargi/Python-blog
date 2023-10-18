78dc5eab5a6739516bbc91d8bd64e680cc3ee687
mr-dargi <dargimohammadreza@gmail.com>
2023-10-18 20:38:51 +0330

    Create form file for receive user forms data and change some information and features

--- account/views.py
+++ account/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from .models import User
+from .forms import ProfileForm
 from .mixins import (
                       FieldsMixin,
                       FormValidMixin,
@@ -44,7 +45,7 @@ class ArticleDelete(SuperUserAccessMixin, DeleteView):
 class Profile(UpdateView):
   model = User
   template_name = "registration/profile.html"
-  fields = ["username","email","first_name","last_name","special_user","is_author"]
+  form_class = ProfileForm
   success_url = reverse_lazy("account:profile")
   
   def get_object(self):
